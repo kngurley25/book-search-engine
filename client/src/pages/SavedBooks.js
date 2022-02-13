148a599ad3ad030cbab677ed4726b896dc320cb2
kngurley <kngurley25@gmail.com>
2022-02-13 16:02:08 -0600

    import queries and mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,6 +5,11 @@ import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { useMutation } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
 
